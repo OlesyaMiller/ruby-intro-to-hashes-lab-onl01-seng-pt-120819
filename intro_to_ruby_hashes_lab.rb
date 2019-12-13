ede3e30133d27e7caf4b9db1589f7c34ef60694b
Olesya Miller <olesyam.miller@gmail.com>
2019-12-13 18:30:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,6 +61,7 @@ def monopoly_with_fourth_tier
   monopoly[:railroads][:names][:shortline] = {}
   
   monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$200"
 
   
   
